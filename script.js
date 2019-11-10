c137b0344063952f9c96da3bb3cc6039fb6aa14b
hhouri9 <hhouri9@geteco.asia>
2019-11-10 11:12:50 +0000

    get data from json file

--- script.js
+++ script.js
@@ -1,7 +1,28 @@
-console.log("hi i am sexy pretty hot and fucking gourgeous Vanilla creamy Java Script");
 
-{
-  var a=4;
-  console.log(a);
+const name = document.querySelector("name");
+
+console.log(`name : ${name}`);
+
+//getElementById('name').
+
+
+const promise1 = function(){
+  return fetch(`./test.json` 
+).then(function(data) { 
+  const json = data.json();
+  console.log(`data.json() : ${json}`);
+  return json;
+});
 }
-console.log(a);
+
+
+promise1()
+.then(
+  function(result)
+  {
+    console.log(result.name);
+    name = result.name;
+  },function(err)
+  {
+    console.log(err);
+  });
