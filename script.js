0372a7b3274532923f87fd3bae280c785e72e28f
hhouri9 <hhouri9@geteco.asia>
2019-11-10 16:56:00 +0000

    -add test2.json -add button click function -add button css layout

--- script.js
+++ script.js
@@ -1,29 +1,58 @@
 
 const name = document.getElementById("name");
+const ability = document.getElementById("ability");
+const region = document.getElementById("region");
 
 console.log(`name : ${name}`);
 //getElementById('name').
 
+document.querySelector('#getProfileButton').addEventListener('click', function () {
+  const promise1 = function () {
+    return fetch(`./test.json`
+    ).then(function (data) {
+      const json = data.json();
+      console.log(`data.json() : ${json}`);
+      return json;
+    });
+  }
 
-const promise1 = function(){
-  return fetch(`./test.json` 
-).then(function(data) { 
-  const json = data.json();
-  console.log(`data.json() : ${json}`);
-  return json;
+  promise1()
+    .then(
+      function (result) {
+        console.log(result.name);
+        // document.querySelector('#name').value = result.name;
+        name.innerHTML = result.name;
+        ability.innerHTML = result.ability;
+        region.innerHTML = result.region;
+
+      }, function (err) {
+        console.log(err);
+      });
 });
-}
 
+document.querySelector('#getProfileButton2').addEventListener('click', function () {
+    console.log(`name : ${name}`);
+    //getElementById('name').
 
-promise1()
-.then(
-  function(result)
-  {
-    console.log(result.name);
-    // document.querySelector('#name').value = result.name;
-    name.innerHTML = result.name;
-    
-  },function(err)
-  {
-    console.log(err);
+    const promise1 = function () {
+      return fetch(`./test2.json`
+      ).then(function (data) {
+        const json = data.json();
+        console.log(`data.json() : ${json}`);
+        return json;
+      });
+    }
+
+    promise1()
+      .then(
+        function (result) {
+          console.log(result.name);
+          // document.querySelector('#name').value = result.name;
+          name.innerHTML = result.name;
+          ability.innerHTML = result.ability;
+          region.innerHTML = result.region;
+
+        }, function (err) {
+          console.log(err);
+        });
   });
