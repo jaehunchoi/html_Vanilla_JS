e16230288db02b43754b2b4b337e2fd1abff058f
hhouri9 <hhouri9@geteco.asia>
2019-11-12 16:32:59 +0000

    modify style

--- sms.js
+++ sms.js
@@ -4,20 +4,25 @@ const sms_b = document.getElementById("sms_b");
 const sms_c = document.getElementById("sms_c");
 const sms_d = document.getElementById("sms_d");
 const sms_e = document.getElementById("sms_e");
+const sms_f = document.getElementById("sms_f");
+const sms_g = document.getElementById("sms_g");
 
 const sms_json = document.getElementById("sms_json");
 
-getJsonFromLocalPath(`./sms.json`);
+getJsonFromLocalPath(`https://raw.githubusercontent.com/swiits/jsontest/master/profile.json`);
 
 document.querySelector('#sms_complete').addEventListener('click', function () {
 //   getJsonFromLocalPath(`./test.json`);
   console.log(`sms_complete`);
   const json = JSON.stringify(
-    {a:sms_a.innerHTML,
-    b:sms_b.value,
-    c:sms_c.value,
-    d:sms_d.value,
-    e:sms_e.value}
+    {name:sms_a.value,
+    type:sms_b.value,
+    region:sms_c.value,
+    description:sms_d.value,
+    // e:sms_e.value,
+    i:sms_e.src,
+    myteam:sms_f.value,
+    owned:sms_g.value}
     );
 
     console.log(json);
@@ -43,13 +48,20 @@ function getJsonFromLocalPath(path) {
   promise()
     .then(
       function (result) {
-        console.log(result.name);
+        const profilesLength = result.profiles.length;
+        const random = Math.floor(Math.random() * profilesLength);
+        console.log(`random : ${random}`);
+        const profile = result.profiles[random];
         // document.querySelector('#name').value = result.name;
-        sms_a.innerHTML = result.a;
-        sms_b.innerHTML = result.b;
-        sms_c.innerHTML = result.c;
-        sms_d.innerHTML = result.d;
-        sms_e.innerHTML = result.e;
+        // sms_a.innerHTML = profile.name;
+        sms_a.value = profile.name;
+        sms_b.value = profile.type;
+        sms_c.value = profile.region;
+        sms_d.value = profile.description;
+        // sms_e.value = profile.img;
+        sms_e.src = profile.img;
+        sms_f.value = profile.myteam;
+        sms_g.value = profile.owned;
       }, function (err) {
         console.log(err);
       });
